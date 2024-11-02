29e75df5f85a9ad5230eb0aa28ce79905d1b2e1f
Seth H <holtzmans@merrimack.edu>
2024-11-01 23:05:24 -0400

    Feat: Added preliminary animations of cards moving to center

--- src/main.js
+++ src/main.js
@@ -10,7 +10,8 @@ import { Game } from './game';
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, .1, 3000);
-camera.position.set(0, 0, 100)  // Try moving this around!
+//camera.position.set(-57, 36, 0)  // Try moving this around!
+camera.position.set(0, 30, 70)
 camera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
 scene.add(camera);
 
@@ -31,31 +32,103 @@ let light = new THREE.AmbientLight(0xFFFFFF, 1)
 scene.add(light)
 
 //create a new table with size 16 (size scaling is still WIP)
-let table = new Table(20);
+let table = new Table(16);
 //add tableGroup(all objects of table)
 scene.add(table.tableGroup);
 
 //create floor
-let floor = new Floor(20);
+let floor = new Floor(16);
 scene.add(floor.mesh)
 
 // Basic light to see materials
 const light2 = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light2);
 
-//add card 
-let card = new Card(5, 'Hearts', '2' )
-card.setPosition(0,25,0)
+let card = new Card(5, 2, 11 )
+card.mesh.rotateX(Math.PI/2)
+card.mesh.rotateZ(Math.PI/2)
+card.setPosition(-30,12.5,0)
 scene.add(card.mesh)
 
-//add deck 
-//let deck = new Deck(scene)
-//deck.shuffle()
+let xAxis = new THREE.Vector3( 0, 1, 0 );
+let qFinal = new THREE.Quaternion().setFromAxisAngle( xAxis, -Math.PI * 2 );
+let quaternionKF = new THREE.QuaternionKeyframeTrack( '.quaternion', [ 0, 2 ], 
+    [ 
+        card.mesh.quaternion.x, card.mesh.quaternion.y, card.mesh.quaternion.z, card.mesh.quaternion.w, 
+        qFinal.x, qFinal.y, qFinal.z, qFinal.w
+    ] );
+let position = new THREE.VectorKeyframeTrack('.position', [0,1,2],
+    [ 
+        -30, 11, 0, 
+        -15, 15, 0, 
+        -7, 12.5, 0 
+    ])
+let clip = new THREE.AnimationClip('action', 3, [ position, quaternionKF])
+let mixer = new THREE.AnimationMixer(card.mesh)
+
+const moveToCenterP1 = mixer.clipAction( clip )
+//moveToCenterP1.loop = THREE.LoopOnce
+moveToCenterP1.clampWhenFinished = true; 
+
+let card2 = new Card(10, 2, 12) 
+card2.mesh.rotateX(Math.PI/2)
+card2.setPosition(0,12.5,25)
+scene.add(card2.mesh)
+
+xAxis = new THREE.Vector3(0, 1, 0)
+qFinal = new THREE.Quaternion().setFromAxisAngle(xAxis, -Math.PI * 2)
+quaternionKF = new THREE.QuaternionKeyframeTrack('.quaternion', [0, 2],
+    [
+        card.mesh.quaternion.x, card.mesh.quaternion.y, card.mesh.quaternion.z, card.mesh.quaternion.w, 
+        qFinal.x, qFinal.y, qFinal.z, qFinal.w
+    ]
+)
+position = new THREE.VectorKeyframeTrack('.position', [0, 1, 2] , 
+    [
+        0 , 11, 25, 
+        0 , 15, 10, 
+        0, 12.5, 0 
+    ])
+let clip2 = new THREE.AnimationClip('action', 3, [ position, quaternionKF])
+let mixer2= new THREE.AnimationMixer(card2.mesh)
+
+const moveToCenterp2 = mixer2.clipAction ( clip2 );
+
+//add card 
+let card3 = new Card(5, 1, 8 )
+card3.mesh.rotateX(Math.PI/2)
+card3.mesh.rotateZ(Math.PI/2)
+card3.setPosition(30,12.5,0)
+scene.add(card3.mesh)
+
+xAxis = new THREE.Vector3( 1, 0, 0 );
+qFinal = new THREE.Quaternion().setFromAxisAngle( xAxis, -Math.PI * 2);
+quaternionKF = new THREE.QuaternionKeyframeTrack( '.quaternion', [ 0, 2 ], [ card3.mesh.quaternion.x, card3.mesh.quaternion.y, card3.mesh.quaternion.z, card3.mesh.quaternion.w, qFinal.x, qFinal.y, qFinal.z, qFinal.w] );
+position = new THREE.VectorKeyframeTrack('.position', [0,1,2],[ 30, 11, 0, 15, 15, 0, 7, 12.5, 0 ])
+let clip3 = new THREE.AnimationClip('action', 3, [ position, quaternionKF])
+let mixer3 = new THREE.AnimationMixer(card3.mesh)
+
+const moveToCenterP3 = mixer3.clipAction( clip3 )
+//moveToCenterP3.loop = THREE.LoopOnce
+moveToCenterP3.clampWhenFinished = true; 
+
+moveToCenterP3.play()
+moveToCenterp2.play()
+moveToCenterP1.play()
+
+const clock = new THREE.Clock();
 
 const game = new Game(scene);
 
 // Animation loop
 function animate() {
+    const delta = clock.getDelta();
+
+    mixer.update( delta );
+    mixer2.update( delta )
+    mixer3.update( delta );
+    
+    
     requestAnimationFrame(animate);
     controls.update(); 
     renderer.render(scene, camera);
